908aef057c56dbedad75dcc8604c4811a2dcefe5
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-02-21 12:19:31 +0100

    3. Creating password contents method.

--- Project 004. Choose password/ViewController.swift
+++ Project 004. Choose password/ViewController.swift
@@ -18,9 +18,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var createPasswordButton: UIButton!
     @IBOutlet weak var changeColorButton: UIButton!
+    
+    // MARK: - Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    private func createRandomValueForPasswordOf(symbolsNumber: Int) -> String {
+        let alphabet = "abcdefghijklmnopqrstuvwxyz"
+        let alphabetUppercased = alphabet.uppercased()
+        let numbers = "0123456789"
+        let symbols = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+        let charactersForPassword = alphabet + alphabetUppercased + numbers + symbols
+        
+        let charactersCount = UInt32(charactersForPassword.count)
+        var passwordString = String()
+        
+        for _ in 0 ..< symbolsNumber {
+            let anyNumber = Int(arc4random_uniform(charactersCount))
+            let anyIndex = charactersForPassword.index(charactersForPassword.startIndex, offsetBy: anyNumber)
+            let incrementedCharacter = charactersForPassword[anyIndex]
+            passwordString += String(incrementedCharacter)
+        }
+        return passwordString
     }
 }
 
