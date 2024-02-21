f6eee4d30f009901de0749fbf1f0ca3084217de7
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-02-21 11:37:15 +0100

    Initial Commit

---
+++ Project 004. Choose password/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 004. Choose password
+//
+//  Created by Mac Alexander on 21.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
