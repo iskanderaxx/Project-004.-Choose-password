8054d49068d200a0f44f0f9b875113eb54a88463
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-02-21 12:17:59 +0100

    2. Creating state variables & UI Outlets.

--- Project 004. Choose password/ViewController.swift
+++ Project 004. Choose password/ViewController.swift
@@ -2,6 +2,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - State
+    
+    private var bruteForcingIsRunning = false
+    
+    private var backgroundIsBlack: Bool = false {
+        didSet { self.view.backgroundColor = backgroundIsBlack ? .black : .white }
+    }
+    
+    // MARK: - UI Elements & Oulets
+    
+    @IBOutlet weak var generatedPasswordLabel: UILabel!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var createPasswordButton: UIButton!
+    @IBOutlet weak var changeColorButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
