9a0578ea3b63b5d4cc0bb361ace97a597436fd1f
Alexander Stepanov <alexanderlawyer97@gmail.com>
2024-02-21 12:01:15 +0100

    1. Configuring files for bruteForce process building.

--- Project 004. Choose password/ViewController.swift
+++ Project 004. Choose password/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  Project 004. Choose password
-//
-//  Created by Mac Alexander on 21.02.2024.
-//
 
 import UIKit
 
@@ -11,9 +5,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
